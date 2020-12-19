a2695cab58521ed1a6c32f37ee9b2f9bfddfd138
Pouya <pouyakhosravi24@gmail.com>
2020-12-19 12:46:48 -0500

    finished base method to transform dataframe data into proper values

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 
-import json, requests
 import pandas as pd
+import numpy as np
 import csv
-import math
 import os
-
+from uncertainties import ufloat, ufloat_fromstr, nominal_value
 
 class DistillationCalculator(object):
 
@@ -17,6 +16,7 @@ class DistillationCalculator(object):
         path = os.path.dirname(os.path.abspath(__file__))
         file_directory = path + '/crudes/crudecomp.CSV'
 
+        # turning our csv fiel in the directory into a pandas DataFrame
         df = pd.read_csv(file_directory)
 
         # grabbing the names of the two oils in question
@@ -27,13 +27,41 @@ class DistillationCalculator(object):
         df.drop(df.columns[-3:], axis=1, inplace=True)      # dropping the last 3 columns that have NaN
         df = df.dropna()                                    # dropping the NaN rows
 
+        oil1_df = df['Unnamed: 1']
+        oil2_df = df['Unnamed: 2']
+
+        # we need to check to see if both series of oils in the df are of equal length
+        if len(oil1_df) == len(oil2_df):
+
+            for index in df.index:
+                # for each value in the first oil and second Series, we will first convert from string to float and the
+                # only take the nominal value from each cell, this will be one of our simplifications
+                try:
+                    df.at[index, 'Unnamed: 1'] = nominal_value(ufloat_fromstr(oil1_df[index]))
+                except ValueError:
+                    df.at[index, 'Unnamed: 1'] = 0
+                try:
+                    df.at[index, 'Unnamed: 2'] = nominal_value(ufloat_fromstr(oil2_df[index]))
+                except ValueError:
+                    df.at[index, 'Unnamed: 2'] = 0
+
+        data = df.to_numpy()
+
+        return data
+
     def getDistilationProfile(self, volume1, volume2):
+
+        """
+        :param volume1: the volume of the first oil the user has specificed in percentage
+        :param volume2: the volume of the second oil the user has specified in percentage
+        :return: approximate temperature of distillation of the mixture of oils at various mass % values
+        """
 
         data = self.get_data()
 
-        volume_total = volume1 + volume2
+        temperature = volume1 + volume2
 
-        return volume_total
+        return temperature
 
     def main(self):
         self.getDistilationProfile(self.volume1, self.volume2)
@@ -42,6 +70,7 @@ class DistillationCalculator(object):
 if __name__ == "__main__":
     print("Starting Distilation Profile Calculator...\n")
     print("Please enter two values for the two oils, please make sure the values add up to 100 in percentage\n")
+
     while True:
         try:
             volume1 = int(input("Please input the volume desired in % for the first oil\n"))
@@ -56,6 +85,7 @@ if __name__ == "__main__":
         except:
             print("Please enter a numeric value\n")
 
+    # converting the user inputted percentages into fraction that will be used later on in the program
     volume1 = int(volume1)/100
     volume2 = int(volume2)/100
 
