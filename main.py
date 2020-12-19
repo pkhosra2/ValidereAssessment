c598e14ed402f3f3cc541e86501d93fc5d52080e
Pouya <pouyakhosravi24@gmail.com>
2020-12-19 15:57:54 -0500

    fixed up variable names and added additional logic to filling missing temperatures in CSV files

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-
 import pandas as pd
 import numpy as np
-import csv
+import math
 import os
-from uncertainties import ufloat, ufloat_fromstr, nominal_value
+from uncertainties import ufloat_fromstr, nominal_value
+
 
 class DistillationCalculator(object):
 
@@ -13,22 +13,22 @@ class DistillationCalculator(object):
 
     def get_data(self):
 
+        # creating our file path for the csv file taken from 'CrudeMonitor.ca'
         path = os.path.dirname(os.path.abspath(__file__))
         file_directory = path + '/crudes/crudecomp.CSV'
 
-        # turning our csv fiel in the directory into a pandas DataFrame
-        df = pd.read_csv(file_directory)
+        # turning our csv file in the directory into a pandas DataFrame
+        df = pd.read_csv(file_directory, header=1)
+
+        # df = df.drop(0)                                    # dropping the first row in the given dataFrame
+        df.drop(df.columns[-3:], axis=1, inplace=True)     # dropping the last 3 columns that have NaN
+        df = df.dropna()                                   # dropping the NaN rows
 
         # grabbing the names of the two oils in question
-        oil_name_1 = df['Unnamed: 1'].values[0]
-        oil_name_2 = df['Unnamed: 2'].values[0]
+        oil_names = [col for col in df.columns[1:]]
 
-        df = df.drop(0)                                     # dropping the first row in the given dataFrame
-        df.drop(df.columns[-3:], axis=1, inplace=True)      # dropping the last 3 columns that have NaN
-        df = df.dropna()                                    # dropping the NaN rows
-
-        oil1_df = df['Unnamed: 1']
-        oil2_df = df['Unnamed: 2']
+        oil1_df = df[oil_names[0]]
+        oil2_df = df[oil_names[1]]
 
         # we need to check to see if both series of oils in the df are of equal length
         if len(oil1_df) == len(oil2_df):
@@ -36,39 +36,49 @@ class DistillationCalculator(object):
             for index in df.index:
                 # for each value in the first oil and second Series, we will first convert from string to float and the
                 # only take the nominal value from each cell, this will be one of our simplifications
+                # NOTE: if there doesn't exist a temperature that the oil is evaporated beyond the previous %,
+                # we can assume that there is no further evaporation that can be done and we can assign the previous
+                # temperature
+
                 try:
-                    df.at[index, 'Unnamed: 1'] = nominal_value(ufloat_fromstr(oil1_df[index]))
+                    df.at[index, oil_names[0]] = nominal_value(ufloat_fromstr(oil1_df[index]))
                 except ValueError:
-                    df.at[index, 'Unnamed: 1'] = 0
+                    df.at[index, oil_names[0]] = df.at[index-1, oil_names[0]]
                 try:
-                    df.at[index, 'Unnamed: 2'] = nominal_value(ufloat_fromstr(oil2_df[index]))
+                    df.at[index, oil_names[1]] = nominal_value(ufloat_fromstr(oil2_df[index]))
                 except ValueError:
-                    df.at[index, 'Unnamed: 2'] = 0
+                    df.at[index, oil_names[1]] = df.at[index-1, oil_names[1]]
 
         data = df.to_numpy()
 
-        return data
+        return data, oil_names
 
     def getDistilationProfile(self, volume1, volume2):
 
         """
+        This method retrieves the clean data and corresponding oil_names from the csv file in the project directory and
+        prints out the respective oil mixture temperatures at the necessary percentage buckets
+
         :param volume1: the volume of the first oil the user has specificed in percentage
         :param volume2: the volume of the second oil the user has specified in percentage
-        :return: approximate temperature of distillation of the mixture of oils at various mass % values
+        :return: None
         """
 
-        data = self.get_data()
+        data, oil_names = self.get_data()
+        print("The resulting Disillation temperatures for {oil_1} and {oil_2} are the following:\n"
+              .format(oil_1=oil_names[0], oil_2=oil_names[1]))
 
-        temperature = volume1 + volume2
-
-        return temperature
+        for array in data:
+            temp = math.floor(array[-1]*volume2 + array[-2]*volume1)
+            print("{mass_value} : {temp}".format(mass_value=array[0], temp=temp))
 
     def main(self):
         self.getDistilationProfile(self.volume1, self.volume2)
 
 
 if __name__ == "__main__":
-    print("Starting Distilation Profile Calculator...\n")
+
+    print("Starting Distillation Profile Calculator...\n")
     print("Please enter two values for the two oils, please make sure the values add up to 100 in percentage\n")
 
     while True:
